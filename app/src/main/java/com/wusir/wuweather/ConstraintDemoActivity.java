7a0d87ce0498911b92d7482a8a5031c52de0ade2
wusir <wujiongping@wjp.com>
2018-02-01 18:05:45 +0800

    xxx

---
+++ app/src/main/java/com/wusir/wuweather/ConstraintDemoActivity.java
@@ -0,0 +1,13 @@
+package com.wusir.wuweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConstraintDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_demo);
+    }
+}
